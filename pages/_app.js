24673d283b318b5be6731602c6af3d3b3250182a
Mate <sergio@cooperativamental.com>
2023-01-04 12:23:23 -0300

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { HostProvider } from "../context/host";
 import "../styles/globals.css";
 import WalletContextProvider from "../context/WalletContext";
 import { PopUpProvider } from "../context/PopUp";
+import { Analytics } from '@vercel/analytics/react';
 
 const MyApp = ({ Component, pageProps }) => {
 
@@ -14,6 +15,7 @@ const MyApp = ({ Component, pageProps }) => {
         <PopUpProvider>
           <Layout>
             <Component {...pageProps} />
+            <Analytics />
           </Layout>
         </PopUpProvider>
       </HostProvider>
