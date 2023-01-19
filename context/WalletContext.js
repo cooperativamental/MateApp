48bbe3dbec34c8140c4468e35c06286908933cfa
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2023-01-19 10:27:43 -0300

    fix wallet disconect

--- context/WalletContext.js
+++ context/WalletContext.js
@@ -1,21 +1,27 @@
-
+import { useMemo } from "react"; 
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import * as Web3 from "@solana/web3.js";
+
 import * as WalletAdapterWallets from "@solana/wallet-adapter-wallets";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 export default function WalletContextProvider({ children }) {
   const endpoint = ["devnet", "mainnet", "testnet"].includes(process.env.NEXT_PUBLIC_NETWORK) ? Web3.clusterApiUrl(process.env.NEXT_PUBLIC_NETWORK) : process.env.NEXT_PUBLIC_NETWORK ?? Web3.clusterApiUrl("devnet");
-  const phantom = new WalletAdapterWallets.PhantomWalletAdapter();
-  const solflare = new WalletAdapterWallets.SolflareWalletAdapter();
+  const wallets = useMemo(
+    () => [
+      new WalletAdapterWallets.PhantomWalletAdapter(),
+      new WalletAdapterWallets.SolflareWalletAdapter()
+    ],
+    []
+  );
 
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={[phantom, solflare]} autoConnect>
+      <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
